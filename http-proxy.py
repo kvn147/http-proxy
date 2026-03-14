36fba25112efab939fd32c8489ac6cd263d05207
wuihee <wuihee@gmail.com>
2026-03-14 00:04:01 -0700

    chore: formatting

--- http-proxy.py
+++ http-proxy.py
@@ -1,6 +1,7 @@
 import socket
-import threading
 import sys
+import threading
+
 
 def run_tcp_server():
     # maybe add base case to check the port from cli is valid
@@ -14,18 +15,19 @@ def run_tcp_server():
     while True:
         client_sock, client_addr = server_sock.accept()
         thread = threading.Thread(
-            target = handle_connection, 
-            args = (client_sock, client_addr)
+            target=handle_connection, args=(client_sock, client_addr)
         )
         thread.start()
 
-def handle_connection (client_sock, client_addr):
+
+def handle_connection(client_sock, client_addr):
     try:
         data = client_sock.recv(4096)
         print(f"Connection from {client_addr}")
-        
+
     finally:
         client_sock.close()
-    
+
+
 if __name__ == "__main__":
     run_tcp_server()
