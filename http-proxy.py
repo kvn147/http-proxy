451eb83822e4a4d37a68abd4d75c7bed6d2cd3cb
wuihee <wuihee@gmail.com>
2026-03-14 19:11:41 -0700

    feat: prepare http request to be sent to server

--- http-proxy.py
+++ http-proxy.py
@@ -44,28 +44,85 @@ def handle_connection(client_sock: socket.socket, client_addr: tuple[str, int]) 
         client_addr: Address (IP, port) bound to the client socket.
     """
     buffer = ""
+    http_request = None
 
     while True:
-        buffer += client_sock.recv(BUFFER_SIZE).decode()
+        bytes_read = client_sock.recv(BUFFER_SIZE).decode()
 
-        # Request line + headers part of a HTTP request will end with \r\n\r\n.
-        raw_request, separator, remaining = buffer.partition("\r\n\r\n")
+        # Connection has been closed.
+        if bytes_read == 0:
+            return
 
-        if not separator:
-            continue
+        buffer += bytes_read
 
-        buffer = remaining
-        http_request = parse_http_request(raw_request)
+        # HTTP request is built and we need the body.
+        if http_request:
+            content_length = int(http_request.headers["Content-Length"])
+
+            if len(buffer) < content_length:
+                continue
+
+            body, buffer = buffer[:content_length], buffer[content_length:]
+            http_request = modify_http_request(http_request)
+            forward_http_request(http_request, body)
+
+            http_request = None
+
+        else:
+            # Request line + headers part of a HTTP request will end with \r\n\r\n.
+            raw_request, separator, remaining = buffer.partition("\r\n\r\n")
+
+            if not separator:
+                continue
+
+            buffer = remaining
+
+            # TODO: Gracefully handle parsing failures, including requests
+            # with no `Content-Length` headers.
+            http_request = serialize_http_request(raw_request)
 
 
-def parse_http_request(raw_request: str) -> HttpRequest:
-    """Extracts the parts of a HTTP request from a raw string.
+def forward_http_request(http_request: HttpRequest, body: str):
+    """Forward a HTTP request to the server from the client.
+
+    Args:
+        http_request: The HTTP request.
+        body: The body of the request.
+    """
+    pass
+
+
+def modify_http_request(http_request: HttpRequest) -> HttpRequest:
+    """Modify a HTTP request to the required format.
+
+    Turn off `Keep-Alive` and lower the HTTP version.
+
+    Args:
+        http_request: The request to transform.
+    """
+    headers = http_request.headers
+
+    if "Connection" in headers:
+        headers["Connection"] = "close"
+
+    if "Proxy-connection" in headers:
+        headers["Proxy-connection"] = "close"
+
+    http_request.protocol = "HTTP/1.0"
+
+    return http_request
+
+
+def serialize_http_request(raw_request: str) -> HttpRequest:
+    """Transform a raw string to a `HttpRequest`.
 
     Args:
         raw_request: The raw string that contains the HTTP request.
     """
     lines = raw_request.splitlines()
     request_line, *header_lines = lines
+
+    print(request_line)
 
     method, uri, protocol = request_line.split()
 
@@ -80,6 +137,17 @@ def parse_http_request(raw_request: str) -> HttpRequest:
             headers[key] = value
 
     return HttpRequest(method, uri, protocol, headers)
+
+
+def deserialize_http_request(http_request: HttpRequest, body: str = "") -> str:
+    """Transform a `HttpRequest` into string format.
+
+    Args:
+        http_request: The request object to transform.
+        body: Optional body of the request.
+    """
+
+    return ""
 
 
 if __name__ == "__main__":
